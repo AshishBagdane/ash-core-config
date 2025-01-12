0f615bd1bfa00b8c27ac3c5a0ef122447fcb401f
Ashish Bagdane <ashishbagdaneofficial@gmail.com>
2025-01-12 11:52:06 +0530

    initial commit

---
+++ src/main/java/dev/ash/core/lib/dtos/EnumMappable.java
@@ -0,0 +1,14 @@
+package dev.ash.core.lib.dtos;
+
+/**
+ * Interface to be implemented by enums that can be converted to EnumDTO. This interface ensures that any enum that should be convertible to EnumDTO provides the necessary getId method.
+ */
+public interface EnumMappable {
+
+  /**
+   * Gets the numeric identifier for the enum value.
+   *
+   * @return the id associated with this enum value
+   */
+  Integer getId();
+}
